775a53e1bbb0aaa9902700439623522be88c4dac
Yifan Wu <yw8012@ic.ac.uk>
2015-11-09 19:36:15 +0000

    Revert "visitor for int overflow"
    This reverts commit a59b5d6fce9931af4571880ef7cdf4ddea7d30ca.

--- src/MyVisitor.java
+++ src/MyVisitor.java
@@ -4,19 +4,8 @@ import antlr.*;
 public class MyVisitor extends BasicParserBaseVisitor<Void> {
 
   public Void visitProgram(BasicParser.ProgramContext ctx) {
-    return visitChildren(ctx);
+    System.out.println("Hi");
+    return null;
   }
 
-  public Void visitIntLiter(BasicParser.IntLiterContext ctx) {
-//    System.out.println("yoyoy");
-    long l = Long.parseLong(ctx.INT_LITER().getSymbol().getText());
-    if (l > Integer.MAX_VALUE)
-    {
-      System.out.println("Integer value " + l + " on line " + ctx.INT_LITER().getSymbol().getLine() + " is too large for a 32-bit signed integer\n" +
-              "Parsing aborted, no further compilation attempted.");
-      System.exit(100);
-    }
-    return visitChildren(ctx); }
-
 }
-
