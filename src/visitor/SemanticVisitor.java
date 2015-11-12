37566fc47e93b3c1d98eae9797535f09439c63b1
Rosita Rodrigues <rr1914@matrix13.doc.ic.ac.uk>
2015-11-12 16:49:46 +0000

    ordered functions according to BasicParser.g4 labels for organisation/clarity

--- src/visitor/SemanticVisitor.java
+++ src/visitor/SemanticVisitor.java
@@ -4,139 +4,9 @@ import antlr.*;
 import symbolTable.SymbolTable;
 
 public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
-
-	private SymbolTable table = new SymbolTable(null);
-
-	@Override
-	public Void visitArgList(BasicParser.ArgListContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitArrayType(BasicParser.ArrayTypeContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitAssignVarStat(BasicParser.AssignVarStatContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitParamList(BasicParser.ParamListContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitExitStat(BasicParser.ExitStatContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitRhsNewPair(BasicParser.RhsNewPairContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitType(BasicParser.TypeContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitBinOpExpr(BasicParser.BinOpExprContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitAssignLhsToRhsStat(
-			BasicParser.AssignLhsToRhsStatContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitLhsPairElem(BasicParser.LhsPairElemContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitSndPairElem(BasicParser.SndPairElemContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPairLiter(BasicParser.PairLiterContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitBeginStat(BasicParser.BeginStatContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitArrayElemExpr(BasicParser.ArrayElemExprContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitFunc(BasicParser.FuncContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitBoolExpr(BasicParser.BoolExprContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitIntLiter(BasicParser.IntLiterContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPairElemArray(BasicParser.PairElemArrayContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitArrayElem(BasicParser.ArrayElemContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitUnOpExpr(BasicParser.UnOpExprContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitParensExpr(BasicParser.ParensExprContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPairPairElem(BasicParser.PairPairElemContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitStatList(BasicParser.StatListContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitReadStat(BasicParser.ReadStatContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitRhsArrayLiter(BasicParser.RhsArrayLiterContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitIdentExpr(BasicParser.IdentExprContext ctx) {
-		return visitChildren(ctx);
-	}
+	
+	private SymbolTable currentTable = new SymbolTable(null);
+	private SymbolTable topTable = new SymbolTable(currentTable);
 
 	@Override
 	public Void visitProgram(BasicParser.ProgramContext ctx) {
@@ -144,32 +14,17 @@ public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
 	}
 
 	@Override
-	public Void visitPairType(BasicParser.PairTypeContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitArrayLiter(BasicParser.ArrayLiterContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitReturnStat(BasicParser.ReturnStatContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
 	public Void visitFuncStat(BasicParser.FuncStatContext ctx) {
 		return visitChildren(ctx);
 	}
-
+	
 	@Override
-	public Void visitPrintStat(BasicParser.PrintStatContext ctx) {
+	public Void visitFunc(BasicParser.FuncContext ctx) {
 		return visitChildren(ctx);
 	}
 
 	@Override
-	public Void visitFstPairElem(BasicParser.FstPairElemContext ctx) {
+	public Void visitParamList(BasicParser.ParamListContext ctx) {
 		return visitChildren(ctx);
 	}
 
@@ -182,6 +37,22 @@ public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
 	public Void visitSkipStat(BasicParser.SkipStatContext ctx) {
 		return visitChildren(ctx);
 	}
+	
+	@Override
+	public Void visitAssignVarStat(BasicParser.AssignVarStatContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitAssignLhsToRhsStat(
+			BasicParser.AssignLhsToRhsStatContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitReadStat(BasicParser.ReadStatContext ctx) {
+		return visitChildren(ctx);
+	}
 
 	@Override
 	public Void visitFreeStat(BasicParser.FreeStatContext ctx) {
@@ -189,47 +60,17 @@ public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
 	}
 
 	@Override
-	public Void visitBoolLiter(BasicParser.BoolLiterContext ctx) {
+	public Void visitReturnStat(BasicParser.ReturnStatContext ctx) {
 		return visitChildren(ctx);
 	}
 
 	@Override
-	public Void visitIdent(BasicParser.IdentContext ctx) {
+	public Void visitExitStat(BasicParser.ExitStatContext ctx) {
 		return visitChildren(ctx);
 	}
 
 	@Override
-	public Void visitLhsArrayElem(BasicParser.LhsArrayElemContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitBaseType(BasicParser.BaseTypeContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPairExpr(BasicParser.PairExprContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitCharLiter(BasicParser.CharLiterContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitRhsFunctionCall(BasicParser.RhsFunctionCallContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPairElemBase(BasicParser.PairElemBaseContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitWhileStat(BasicParser.WhileStatContext ctx) {
+	public Void visitPrintStat(BasicParser.PrintStatContext ctx) {
 		return visitChildren(ctx);
 	}
 
@@ -239,22 +80,22 @@ public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
 	}
 
 	@Override
-	public Void visitBinaryOper(BasicParser.BinaryOperContext ctx) {
+	public Void visitIfThenElseStat(BasicParser.IfThenElseStatContext ctx) {
 		return visitChildren(ctx);
 	}
 
 	@Override
-	public Void visitRhsExpr(BasicParser.RhsExprContext ctx) {
+	public Void visitWhileStat(BasicParser.WhileStatContext ctx) {
 		return visitChildren(ctx);
 	}
 
 	@Override
-	public Void visitCharExpr(BasicParser.CharExprContext ctx) {
+	public Void visitBeginStat(BasicParser.BeginStatContext ctx) {
 		return visitChildren(ctx);
 	}
 
 	@Override
-	public Void visitUnaryOper(BasicParser.UnaryOperContext ctx) {
+	public Void visitStatList(BasicParser.StatListContext ctx) {
 		return visitChildren(ctx);
 	}
 
@@ -264,7 +105,87 @@ public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
 	}
 
 	@Override
+	public Void visitLhsArrayElem(BasicParser.LhsArrayElemContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitLhsPairElem(BasicParser.LhsPairElemContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitRhsExpr(BasicParser.RhsExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitRhsArrayLiter(BasicParser.RhsArrayLiterContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitRhsNewPair(BasicParser.RhsNewPairContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
 	public Void visitRhsPairElem(BasicParser.RhsPairElemContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitRhsFunctionCall(BasicParser.RhsFunctionCallContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitArgList(BasicParser.ArgListContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitFstPairElem(BasicParser.FstPairElemContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitSndPairElem(BasicParser.SndPairElemContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitType(BasicParser.TypeContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitBaseType(BasicParser.BaseTypeContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitArrayType(BasicParser.ArrayTypeContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitPairType(BasicParser.PairTypeContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitPairElemBase(BasicParser.PairElemBaseContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitPairElemArray(BasicParser.PairElemArrayContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitPairPairElem(BasicParser.PairPairElemContext ctx) {
 		return visitChildren(ctx);
 	}
 
@@ -274,7 +195,12 @@ public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
 	}
 
 	@Override
-	public Void visitIfThenElseStat(BasicParser.IfThenElseStatContext ctx) {
+	public Void visitBoolExpr(BasicParser.BoolExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitCharExpr(BasicParser.CharExprContext ctx) {
 		return visitChildren(ctx);
 	}
 
@@ -282,9 +208,84 @@ public class SemanticVisitor extends BasicParserBaseVisitor<Void> {
 	public Void visitStringExpr(BasicParser.StringExprContext ctx) {
 		return visitChildren(ctx);
 	}
+	
+	@Override
+	public Void visitPairExpr(BasicParser.PairExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitIdentExpr(BasicParser.IdentExprContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitArrayElemExpr(BasicParser.ArrayElemExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitUnOpExpr(BasicParser.UnOpExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitBinOpExpr(BasicParser.BinOpExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitParensExpr(BasicParser.ParensExprContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitUnaryOper(BasicParser.UnaryOperContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitBinaryOper(BasicParser.BinaryOperContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitIdent(BasicParser.IdentContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitArrayElem(BasicParser.ArrayElemContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitIntLiter(BasicParser.IntLiterContext ctx) {
+		return visitChildren(ctx);
+	}
+
+	@Override
+	public Void visitBoolLiter(BasicParser.BoolLiterContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitCharLiter(BasicParser.CharLiterContext ctx) {
+		return visitChildren(ctx);
+	}
 
 	@Override
 	public Void visitStringLiter(BasicParser.StringLiterContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitArrayLiter(BasicParser.ArrayLiterContext ctx) {
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public Void visitPairLiter(BasicParser.PairLiterContext ctx) {
 		return visitChildren(ctx);
 	}
 }
