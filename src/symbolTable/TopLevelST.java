0b764a185e068ac90dbc037c3692dcafa1ecf2ab
Daniel Budiono <db3014@vm-shell1.doc.ic.ac.uk>
2015-11-12 21:44:46 +0000

    Fix TopLevelST to exclude function

--- src/symbolTable/TopLevelST.java
+++ src/symbolTable/TopLevelST.java
@@ -12,12 +12,13 @@ public class TopLevelST {
     ST.add("char", charT);
     ST.add("bool", new Scalar(0, 1));
     ST.add("string", new Array(charT, 2);
-
+    /*
     doubleT = ST.lookup("double");
     ST.add("sin", new Function(returntype=doubleT,formals=new Param(doubleT));
     arrayT = ST.lookup("array")
     ST.add("len", new Function(returntype="int", formals=new Param(arrayT)));
     ST.add("!", new Function(returntype="bool", formals=new Param("bool")));
+    */
 }
 
 
