139e426db5e0bb1ddf87bc29936d630bf99e7be9
Yifan Wu <yw8012@ic.ac.uk>
2015-11-12 15:07:06 +0000

    Remove Class and Package identifier

--- src/symbolTable/Identifier.java
+++ src/symbolTable/Identifier.java
@@ -1,46 +1,39 @@
 package symbolTable;
 
-public class Identifier {}
+public class Identifier {
+}
 
 class Type extends Identifier {
-	public boolean isDeclarable() {
-		return true;
-	}
+  public boolean isDeclarable() {
+    return true;
+  }
 }
 
 class Variable extends Identifier {
-	Type type;
+  Type type;
 }
 
 class Param extends Identifier {
-	Type type;
+  Type type;
 }
 
 class Scalar extends Type {
-	int min;
-	int max;
-	public Scalar(int min, int max) {
-		this.min = min;
-		this.max = max;
-	}
+  int min;
+  int max;
+
+  public Scalar(int min, int max) {
+    this.min = min;
+    this.max = max;
+  }
 }
 
 class Array extends Type {
-	Type type;
-	int numOfElems;
-}
-
-class Package extends Identifier {
-	SymbolTable table;
-}
-
-class Class extends Type {
-	Class superClass;
-	SymbolTable table;
+  Type type;
+  int numOfElems;
 }
 
 class Function extends Identifier {
-	Type returnType;
-	Param params[];
-	SymbolTable table;
+  Type returnType;
+  Param params[];
+  SymbolTable table;
 }
