3ec41dc92af4a1db51cf4c74d7724e0a33b4c9a7
Max Dudzinski <md3414@corona28.doc.ic.ac.uk>
2015-11-16 16:05:16 +0000

    MultiMap in ST

--- src/wacc/symboltable/SymbolTable.java
+++ src/wacc/symboltable/SymbolTable.java
@@ -2,20 +2,25 @@ package wacc.symboltable;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.List;
 
+import antlr.BasicParser;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.MultiMap;
+import wacc.visitor.utils.Utils;
 
 public class SymbolTable {
 
   private final SymbolTable encSymTable;
-  private final Dictionary<String, ParserRuleContext> dict = new Hashtable<>();
+  private final MultiMap<String, ParserRuleContext> dict = new MultiMap<>();
+ // private final Dictionary<String, ParserRuleContext> dict = new Hashtable<>();
 
   public SymbolTable(SymbolTable st) {
     encSymTable = st;
   }
 
   public void add(String name, ParserRuleContext object) {
-    dict.put(name, object);
+    dict.map(name, object);
   }
 
   public SymbolTable getEncSymTable() {
@@ -23,7 +28,22 @@ public class SymbolTable {
   }
 
   public ParserRuleContext lookup(String name) {
-    return dict.get(name);
+    if (dict.get(name) != null)
+      return dict.get(name).get(0);
+    else
+      return null;
+   // return dict.get(name);
+  }
+
+  public ParserRuleContext lookup(String name, BasicParser.TypeContext type) {
+    List<ParserRuleContext> entries = dict.get(name);
+    for (ParserRuleContext entry : entries) {
+      if (entry instanceof BasicParser.TypeContext)
+        if (Utils.getType((BasicParser.TypeContext)entry).equals(Utils.getType(type))) {
+        return entry;
+      }
+    }
+    return null;
   }
 
   public ParserRuleContext lookupAll(String name) {
