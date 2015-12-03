53d8509fb3aac727d0fdd8ac36098d6df57a9573
Yifan Wu <yw8012@ic.ac.uk>
2015-12-03 13:54:48 +0000

    Implement SymbolTable

---
+++ src/wacc/visitor/code_generator/SymbolTable.java
@@ -0,0 +1,35 @@
+package wacc.visitor.code_generator;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class SymbolTable {
+
+  private SymbolTable encSymTable;
+  private final Dictionary<String, Integer> dict = new Hashtable<>();
+
+  public SymbolTable(SymbolTable st) {
+    encSymTable = st;
+  }
+
+  public void add(String name, Integer obj) {
+    dict.put(name, obj);
+  }
+
+  public Integer lookup(String name) {
+    return dict.get(name);
+  }
+
+  public Integer lookupAll(String name) {
+    SymbolTable s = this;
+    do {
+      Integer obj = s.lookup(name);
+      if (obj != null) {
+        return obj;
+      }
+      s = s.encSymTable;
+    } while (s != null);
+    return null;
+  }
+
+}
